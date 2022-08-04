61cf7e94159dba5d8930ada412738bb537e185c4
OscarCubeles <oscar.cubeles@students.salle.url.edu>
2022-08-04 19:52:02 +0200

    Single Page App Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,8 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
@@ -53,7 +55,10 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FontAwesomeModule,
     RouterModule
   ],
-  providers: [DishService],
+  providers: [
+    DishService,
+    PromotionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Dish } from '../shared/dish';
+import { DishService } from '../services/dish.service';
+import { Promotion } from '../shared/promotion';
+import { PromotionService } from '../services/promotion.service';
+import { PrintHook } from '@angular/flex-layout';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  dish: Dish;
+  promotion: Promotion;
+
+  constructor(private dishservice: DishService, private promotionservice: PromotionService) { 
+    this.dish = this.dishservice.getFeaturedDish();
+    this.promotion = this.promotionservice.getFeaturedPromotion();
+  }
 
   ngOnInit(): void {
+    this.dish = this.dishservice.getFeaturedDish();
+    this.promotion = this.promotionservice.getFeaturedPromotion();
   }
 
 }
